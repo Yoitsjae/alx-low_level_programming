684ae91e372cf9cd026630a4069c4bef38dd0772
yoitsjae <ncubejason@gmail.com>
2023-02-23 15:06:09 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_numbers - checks for checks for a digit (0 through 9).
+ *  main - check the code (0 through 9).
  *
  * Return: Always 0.
  */
-void print_numbers(void)
+int main(void)
 {
 int c;
 
