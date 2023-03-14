913314be0e63ded807d9a84f8665c8fb78ba84e4
yoitsjae <ncubejason@gmail.com>
2023-03-14 12:00:18 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -4,7 +4,6 @@
 /**
  * count_word - helper function to count the number of words in a string
  * @s: string to evaluate
- *
  * Return: number of words
  */
 int count_word(char *s)
@@ -19,20 +18,16 @@ int count_word(char *s)
 		if (s[c] == ' ')
 			flag = 0;
 
-	else if (flag == 0)
-	}
 		{
 flag = 1;
 			w++;
 		}
-
-
+}
 	return (w);
 }
 /**
  * **strtow - splits a string into words
  * @str: string to split
- *
  * Return: pointer to an array of strings (Success)
  * or NULL (Error)
  */
