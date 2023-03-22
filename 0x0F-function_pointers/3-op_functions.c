486c107427c4559914db48fa4c3d2b40b16ba74b
yoitsjae <ncubejason@gmail.com>
2023-03-22 13:00:13 -0700

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -17,13 +17,13 @@ int op_mod(int a, int b);
  */
 int op_add(int a, int b)
 {
-return (a + b);
+	return (a + b);
 }
 /**
  * op_sub - Returns the difference of two numbers.
  * @a: The first number.
  * @b: The second number.
- *
+*
  * Return: The difference of a and b.
  */
 int op_sub(int a, int b)
